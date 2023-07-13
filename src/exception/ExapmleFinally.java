c4ac72d13468d5965933082261fb65791bb9838c
AnastasiaT <a.tambovceva@okeanprobor.ru>
2023-07-13 15:17:26 +0300

    Java feat: Finally example

---
+++ src/exception/ExapmleFinally.java
@@ -0,0 +1,15 @@
+package exception;
+
+public class ExapmleFinally {
+    public static void main(String[] args) {
+        try{
+            String data = null;
+            System.out.println(data);
+        } catch (NullPointerException e){
+            System.out.println(e);
+        } finally {
+            System.out.println("finally block");
+        }
+        System.out.println("code next try/catch");
+    }
+}
